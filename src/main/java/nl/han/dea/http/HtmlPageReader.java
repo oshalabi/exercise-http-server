5893cca1e846681d93c38b3e15decff107220660
Meron Brouwer <meron.brouwer@gmail.com>
2019-07-18 13:03:33 +0200

    Added correct length calculations

--- src/main/java/nl/han/dea/http/HtmlPageReader.java
+++ src/main/java/nl/han/dea/http/HtmlPageReader.java
@@ -3,20 +3,32 @@ package nl.han.dea.http;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class HtmlPageReader {
-    public String readFile(String filename) {
-        var fullFileName = "pages/".concat(filename);
-        try {
-            ClassLoader classLoader = getClass().getClassLoader();
+    public String readFile(String filename) throws IOException {
+        var path = getPath(filename);
 
-            var file = new File(classLoader.getResource(fullFileName).getFile()).toPath();
+        var fileAsString = new String(Files.readAllBytes(path));
 
-            var fileAsString = new String(Files.readAllBytes(file));
+        return fileAsString;
 
-            return fileAsString;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    }
+
+    public String getLength(String filename) {
+        if (filename.isEmpty()) {
+            return "0";
         }
+
+        var path = getPath(filename);
+        var length = path.toFile().length();
+
+        return Long.toString(length);
+    }
+
+    private Path getPath(String filename) {
+        var fullFileName = "pages/".concat(filename);
+        var classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource(fullFileName).getFile()).toPath();
     }
 }
