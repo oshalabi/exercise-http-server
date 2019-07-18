1b5755068b4955b7a8ba4a46eb48a717e2e0794d
Meron Brouwer <meron.brouwer@gmail.com>
2019-07-18 12:34:16 +0200

    Updated the code to Java12 and made the code a more verbose

--- src/main/java/nl/han/dea/http/ConnectionHandler.java
+++ src/main/java/nl/han/dea/http/ConnectionHandler.java
@@ -9,18 +9,21 @@ public class ConnectionHandler {
     private static final String HTTP_HEADER = "HTTP/1.1 200 OK\n" +
             "Date: Mon, 27 Aug 2018 14:08:55 +0200\n" +
             "HttpServer: Simple DEA Webserver\n" +
-            "Content-Length: 106\n" +
+            "Content-Length: 190\n" +
             "Content-Type: text/html\n";
 
     private static final String HTTP_BODY = "<!DOCTYPE html>\n" +
-            "<html>\n" +
+            "<html lang=\"en\">\n" +
+            "<head>\n" +
+            "<meta charset=\"UTF-8\">\n" +
+            "<title>Simple Http Server</title>\n" +
+            "</head>\n" +
             "<body>\n" +
             "<h1>Hi DEA folks!</h1>\n" +
             "<p>This is a simple line in html.</p>\n" +
             "</body>\n" +
             "</html>\n" +
             "\n";
-
     private Socket socket;
 
     public ConnectionHandler(Socket socket) {
@@ -30,25 +33,29 @@ public class ConnectionHandler {
 
     public void handle() {
         try {
-            BufferedReader inputStreamReader = new BufferedReader(
+            var inputStreamReader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream(), StandardCharsets.US_ASCII));
-            BufferedWriter outputStreamWriter = new BufferedWriter(
+            var outputStreamWriter = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.US_ASCII));
-            String requestLine;
-            while ((requestLine = inputStreamReader.readLine()) != null) {
-                System.out.println(requestLine);
-                if (lineMarksEndOfRequest(requestLine)) {
-                    writeResponseMessage(outputStreamWriter);
-                    break;
-                }
-            }
+
+            parseRequest(inputStreamReader);
+            writeResponse(outputStreamWriter);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    private void writeResponseMessage(BufferedWriter outputStreamWriter) {
+    private void parseRequest(BufferedReader inputStreamReader) throws IOException {
+        var request = inputStreamReader.readLine();
+
+        while (request != null && !request.isEmpty()) {
+            System.out.println(request);
+            request = inputStreamReader.readLine();
+        }
+    }
+
+    private void writeResponse(BufferedWriter outputStreamWriter) {
         try {
             outputStreamWriter.write(HTTP_HEADER);
             outputStreamWriter.newLine();
@@ -60,9 +67,4 @@ public class ConnectionHandler {
         }
 
     }
-
-    private boolean lineMarksEndOfRequest(String line) {
-        return line.isEmpty();
-    }
-
 }
--- src/main/java/nl/han/dea/http/HttpServer.java
+++ src/main/java/nl/han/dea/http/HttpServer.java
@@ -17,9 +17,10 @@ public class HttpServer {
 
     public void startServer() {
         try (
-                ServerSocket serverSocket = new ServerSocket(this.tcpPort);
+                var serverSocket = new ServerSocket(this.tcpPort);
         ) {
             System.out.println("Server accepting requests on port " + tcpPort);
+
             new ConnectionHandler(serverSocket.accept()).handle();
         } catch (IOException e) {
             throw new RuntimeException(e);
